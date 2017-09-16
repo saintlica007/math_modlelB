eaa941fde83db866608568e75f8c3276433193c9
saintlica007 <10164700130@stu.ecnu.edu.cn>
2017-09-16 18:44:09 +0800

    321
    4124124

--- python程序/zb2.py
+++ python程序/zb2.py
@@ -3,9 +3,10 @@ s=[]
 for i in l:
     k=i.split()
     k[0],k[1]=k[1],k[0]
-    b=",".join(k)
+    #if(float(k[0])>110.5) and (float(k[0])<114.5) and (float(k[1])>22.3) and (float(k[1])<25):
+    b="\t".join(k)
     s.append(b)
-f=open("zb2cl.txt","w")
+f=open("zb2cl2.txt","w")
 f.write("\n".join(s))
 f.close()
 
---
+++ python程序/聚类.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from sklearn.cluster import KMeans
+from sklearn.externals import joblib
+import numpy
+import time
+import matplotlib.pyplot as plt
+ 
+if __name__ == '__main__':
+    ## step 1: 加载数据
+    print("step 1: load data...")
+    dataSet = []
+    fileIn = open('zb2cl3.txt')
+    for line in fileIn.readlines():
+        lineArr = line.strip().split()
+        dataSet.append([float(lineArr[0]), float(lineArr[1])])
+ 
+    #设定不同k值以运算
+    for k in range(7,8):
+        clf = KMeans(n_clusters=k) #设定k  ！！！！！！！！！！这里就是调用KMeans算法
+        s = clf.fit(dataSet) #加载数据集合
+        numSamples=len(dataSet)
+        centroids = clf.labels_
+        print(centroids,type(centroids)) #显示中心点
+        print(clf.inertia_)  #显示聚类效果
+        mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
+        #画出所有样例点 属于同一分类的绘制同样的颜色
+        for i in range(numSamples):
+            #markIndex = int(clusterAssment[i, 0])
+            plt.plot(dataSet[i][0], dataSet[i][1], mark[clf.labels_[i]]) #mark[markIndex])
+            f=open("聚类%d.txt"%(clf.labels_[i]),"a")
+            f.write("%d"%(i)+"\n")
+            f.close()
+        mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
+        # 画出质点，用特殊图型
+        centroids =  clf.cluster_centers_
+        for i in range(k):
+            plt.plot(centroids[i][0], centroids[i][1], mark[i], markersize = 12)
+            #print centroids[i, 0], centroids[i, 1]
+        plt.show()
